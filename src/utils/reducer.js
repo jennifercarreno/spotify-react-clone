8f29b2b1209ed01df38619303624e65d94302741
Jennifer Carreno <jennifercarreno72@gmail.com>
2023-02-28 00:52:34 -0800

    navbar

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -2,7 +2,8 @@ import { reducerCases } from "./Constants";
 
 export const initalState = {
     token:null,
-    playlists: []
+    playlists: [],
+    userInfo: null,
 };
 
 const reducer = (state, action) => {
@@ -18,6 +19,12 @@ const reducer = (state, action) => {
                 ...state,
                 playlists: action.playlists,
 
+            }
+        }
+        case reducerCases.SET_USER : {
+            return {
+                ...state,
+                userInfo: action.userInfo,
             }
         }
         default:
