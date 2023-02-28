abfacd3bf6e8d58df730fcf577bae007e85bc592
Jennifer Carreno <jennifercarreno72@gmail.com>
2023-02-27 21:00:07 -0800

    spotify login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import Login from './components/Login';
+import Spotify from './components/Spotify';
+import { reducerCases } from './utils/Constants';
+import { useStateProvider } from './utils/StateProvider';
 
 export default function App() {
+
+  const [{ token }, dispatch] = useStateProvider();
+  console.log(token)
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      console.log("Token: " + token);
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+
+    }
+  }, [token, dispatch]);
+
   return (
-    <div>App</div>
+    <div>
+      { token ? <Spotify /> : <Login />}
+    </div>
   )
 }
