b0ecbfa5eaa05b81bcef3e477bb276923f764b16
Jennifer Carreno <jennifercarreno72@gmail.com>
2023-02-27 19:58:23 -0800

    init commit, base setup

---
+++ src/utils/reducer.js
@@ -0,0 +1,12 @@
+export const initalState = {
+    token:"spotify token added later",
+};
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
+
+export default reducer;
