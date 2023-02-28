75a4bd6704c5477881decd8e3a7f5c2db5200469
Jennifer Carreno <jennifercarreno72@gmail.com>
2023-02-28 15:25:39 -0800

    body done and styled, need to fix nav

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -4,6 +4,8 @@ export const initalState = {
     token:null,
     playlists: [],
     userInfo: null,
+    selectedPlaylistId: "41x4PJTfEAgk1tngk1FRud",
+    selectedPlaylist: null,
 };
 
 const reducer = (state, action) => {
@@ -25,6 +27,12 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 userInfo: action.userInfo,
+            }
+        }
+        case reducerCases.SET_PLAYLIST : {
+            return {
+                ...state,
+                selectedPlaylist: action.selectedPlaylist,
             }
         }
         default:
